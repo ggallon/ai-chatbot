7f8b0371e7f2f6b7d7399f638d4e7675dba1aa56
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-19 11:58:01 +0200

    switch @vercel/kv to @upstash/redis

--- app/actions.ts
+++ app/actions.ts
@@ -2,9 +2,9 @@
 
 import { revalidatePath } from 'next/cache'
 import { redirect } from 'next/navigation'
-import { kv } from '@vercel/kv'
 
 import { auth } from '@/auth'
+import { kv } from '@/lib/upstash/redis'
 import { type Chat } from '@/lib/types'
 
 export async function getChats(userId?: string | null) {
--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -1,8 +1,8 @@
-import { kv } from '@vercel/kv'
 import { OpenAIStream, StreamingTextResponse } from 'ai'
 import { Configuration, OpenAIApi } from 'openai-edge'
 
 import { auth } from '@/auth'
+import { kv } from '@/lib/upstash/redis'
 import { nanoid } from '@/lib/utils'
 
 export const runtime = 'edge'
