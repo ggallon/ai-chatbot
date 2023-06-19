ce12f9e5db45ac0f76a41590080a986ba0293d0d
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-19 12:11:02 +0200

    fix type error

--- lib/upstash/redis/index.ts
+++ lib/upstash/redis/index.ts
@@ -2,6 +2,6 @@ import { Redis } from "@upstash/redis"
 
 // Initiate Redis instance by connecting to REST URL
 export const kv = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN,
+  url: process.env.UPSTASH_REDIS_REST_URL || '',
+  token: process.env.UPSTASH_REDIS_REST_TOKEN || '',
 })
