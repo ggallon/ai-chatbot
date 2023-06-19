3cca80c055ab441ab5d95f98e8f6b27a504a2c94
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-19 12:11:52 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -4,4 +4,7 @@ module.exports = {
   experimental: {
     serverActions: true,
   },
+  /** We already do linting and typechecking as separate tasks in CI */
+  eslint: { ignoreDuringBuilds: true },
+  typescript: { ignoreBuildErrors: true },
 };
