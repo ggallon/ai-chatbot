32b26a6f8f81e852e1063b0c70beb37bad9038d6
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-19 17:58:20 +0200

    Update page.tsx

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,7 @@
 import { Suspense } from 'react'
 import { LoginButton } from '@/components/login-button'
 
-export default async function SignInPage() {
+export default function SignInPage() {
   return (
     <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
       <Suspense fallback={null}>
