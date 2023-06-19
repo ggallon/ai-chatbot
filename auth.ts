39f4efdfdef519667d0f2bc57bd4e413c31a1434
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-19 17:37:05 +0200

    Update auth.ts

--- auth.ts
+++ auth.ts
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import NextAuth from 'next-auth'
 import GitHub from 'next-auth/providers/github'
 
@@ -10,31 +11,33 @@ export const {
   // @ts-ignore
   debug: true,
   providers: [GitHub],
+  session: {
+    strategy: "jwt",
+    // Seconds - How long until an idle session expires and is no longer valid.
+    maxAge: 7 * 24 * 60 * 60, // 7 days
+    // Seconds - Throttle how frequently to write to database to extend a session.
+    // Use it to limit write operations. Set to 0 to always update the database.
+    // Note: This option is ignored if using JSON Web Tokens
+    updateAge: 24 * 60 * 60, // 24 hours
+  },
   callbacks: {
-    signIn(signIndata) {
-      console.log("callbacks::signIn", signIndata);
-      return true;
-    },
-    // @ts-ignore
-    jwt: async (jwtdata) => {
-      console.log("callbacks::jwt", jwtdata);
-      const { token, profile } = jwtdata;
-      if (profile?.id) {
+    jwt(jwtdata) {
+      const { token, profile, trigger } = jwtdata;
+      if (trigger === "signIn" && profile?.id) {
         token.id = profile.id
-        token.image = profile.picture
+        token.image = profile.avatar_url
       }
       return token
     },
-    session(sessionData) {
-      console.log("callbacks::session", sessionData);
-      return sessionData.session;
-    },
     authorized({ request, auth }) {
-      console.log("callbacks::authorized", auth);
-      const domain = request.headers.get("host");
-      console.log("domain", domain);
+      const session = auth.user;
+      const path = request.nextUrl.pathname;
 
-      return !!auth.user
+      if (!session && path !== "/sign-in") {
+        return NextResponse.redirect(new URL("/sign-in", request.url));
+      } else if (session && path === "/sign-in") {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
     }
   },
   pages: {
