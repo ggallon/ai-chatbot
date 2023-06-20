1909744f3f41e08f55ea3588988abed96ae28813
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-20 14:19:07 +0200

    styles

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -4,7 +4,7 @@ import { LoginButton } from '@/components/login-button'
 
 export default function SignInPage() {
   return (
-    <div className="flex h-[calc(100vh-theme(spacing.16))] items-center justify-center py-10">
+    <div className="flex h-[calc(100vh-theme(spacing.16))] flex-col items-center justify-center gap-5 py-10">
       <Suspense fallback={null}>
         <LoginButton />
       </Suspense>
