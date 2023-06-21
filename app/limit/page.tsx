99787604d61087a9339f3dd6472fe7b1c9e75372
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-21 02:38:31 +0200

    Update page.tsx

--- app/limit/page.tsx
+++ app/limit/page.tsx
@@ -32,7 +32,7 @@ export default async function Home() {
           </p>
         </div>
 
-        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30">
+        <div className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30">
           <h2 className={'mb-3 text-2xl font-semibold'}>Limit </h2>
           <p className={'m-0 max-w-[30ch] text-sm opacity-50'}>{limit}</p>
         </div>
