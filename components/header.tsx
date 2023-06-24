e7e4bdc258c1dee7e6750262bf47eca0e669338b
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-24 18:25:28 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -45,7 +45,7 @@ export async function Header() {
             <UserMenu user={session.user} />
           ) : (
             <Button variant="link" asChild className="-ml-2">
-              <Link href="/sign-in?callbackUrl=/">Login</Link>
+              <Link href="/sign-in">Login</Link>
             </Button>
           )}
         </div>
