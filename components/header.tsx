6f442c3944d9d35e70212d43be3dd67a326a4ac6
Gwenaël Gallon <gwenael.gallon@mac.com>
2023-06-25 04:02:04 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,7 @@ import { cn } from '@/lib/utils/ui'
 export async function Header() {
   const session = await auth()
   return (
-    <header className="sticky top-0 z-50 flex h-16 w-full shrink-0 items-center justify-between gap-3 border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl sm:gap-3.5">
+    <header className="sticky top-0 z-50 flex h-11 w-full shrink-0 items-center justify-between gap-3 border-b bg-gradient-to-b from-background/10 via-background/50 to-background/80 px-4 backdrop-blur-xl sm:gap-3.5">
       <div className="ai-effect inline-flex select-none items-center whitespace-nowrap">
         {session?.user ? (
           <Sidebar>
@@ -37,7 +37,7 @@ export async function Header() {
         <IconSeparator className="-mr-1 h-6 w-6 text-muted-foreground/50" />
         <Link href="/">
           <span className="font-bold lg:text-lg">
-            <IconAi className="mb-0.5 mr-0 inline w-4 text-black sm:w-5" />
+            <IconAi className="mb-0.5 mr-0 inline w-4 text-black text-foreground sm:w-5" />
             AI
           </span>
         </Link>
