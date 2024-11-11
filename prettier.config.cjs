3c8b93d422ed5535f8b0c0bc9a5dfff00f6eee24
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-11-11 02:39:51 +0100

    Update prettier.config.cjs

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,11 +1,11 @@
 /** @type {import('prettier').Config} */
 module.exports = {
   printWidth: 80,
-  endOfLine: 'lf',
+  endOfLine: "lf",
   semi: true,
   useTabs: false,
   singleQuote: false,
   tabWidth: 2,
-  trailingComma: 'es5',
+  trailingComma: "es5",
   bracketSpacing: true,
 };
