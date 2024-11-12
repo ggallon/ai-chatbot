fdfc0a45f4131f4c71fb58d2d8a7fff65f5ce666
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-11-12 23:52:27 +0100

    Replace deprecated ElementRef to ComponentRef

--- components/ui/sidebar.tsx
+++ components/ui/sidebar.tsx
@@ -333,7 +333,7 @@ const SidebarInset = React.forwardRef<
 SidebarInset.displayName = "SidebarInset"
 
 const SidebarInput = React.forwardRef<
-  React.ElementRef<typeof Input>,
+  React.ComponentRef<typeof Input>,
   React.ComponentProps<typeof Input>
 >(({ className, ...props }, ref) => {
   return (
@@ -381,7 +381,7 @@ const SidebarFooter = React.forwardRef<
 SidebarFooter.displayName = "SidebarFooter"
 
 const SidebarSeparator = React.forwardRef<
-  React.ElementRef<typeof Separator>,
+  React.ComponentRef<typeof Separator>,
   React.ComponentProps<typeof Separator>
 >(({ className, ...props }, ref) => {
   return (
