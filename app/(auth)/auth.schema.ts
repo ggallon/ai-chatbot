0a5d8c1be2d0a3938bf19dd2a2ff2063a17f17b3
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-11-24 01:06:01 +0100

    Force email tp lowercase

--- app/(auth)/auth.schema.ts
+++ app/(auth)/auth.schema.ts
@@ -1,6 +1,6 @@
 import { z } from "zod";
 
 export const authFormSchema = z.object({
-  email: z.string().email().min(3).max(255).trim(),
+  email: z.string().trim().toLowerCase().email().min(5).max(255),
   password: z.string().min(8).max(70).trim(),
 });
