4b1a18eae329eaf7fb0f495de877885daa69c4c5
Gwenaël Gallon <gwenael.gallon@mac.com>
2024-12-08 13:16:58 +0100

    feat: update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -5,5 +5,15 @@ import { authConfig } from '@/app/(auth)/auth.config';
 export default NextAuth(authConfig).auth;
 
 export const config = {
-  matcher: ['/', '/:id', '/api/:path*', '/login', '/register'],
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - chat (APP routes) for public shared chat
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - .well-known/*, favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    '/((?!api|chat|_next/static|_next/image|.well-known|favicon|fonts|sitemap|robots.txt).*)',
+  ],
 };
