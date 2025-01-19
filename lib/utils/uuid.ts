659fe16f7b4edddfec48feae59da4d3c9892afae
Gwenaël Gallon <gwenael.gallon@mac.com>
2025-01-19 21:12:41 +0100

    feat: use crypto.randomUUID

--- lib/utils/uuid.ts
+++ lib/utils/uuid.ts
@@ -1,4 +1,13 @@
-export function generateUUID(): string {
+/**
+ * Generate a UUID v4 based on random numbers.
+ *
+ * @return The generate UUID
+ */
+export function generateUUID() {
+  if (typeof self.crypto.randomUUID === typeof Function) {
+    return self.crypto.randomUUID();
+  }
+
   return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, (c) => {
     const r = (Math.random() * 16) | 0;
     const v = c === 'x' ? r : (r & 0x3) | 0x8;
