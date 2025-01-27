c780cb1feca6077ccb87be4704c3659750bb8cfe
Gwenaël Gallon <gwenael.gallon@mac.com>
2025-01-27 20:14:03 +0100

    feat: add abortSignal to weather tool

--- lib/ai/tools/get-weather.ts
+++ lib/ai/tools/get-weather.ts
@@ -16,11 +16,12 @@ export const getWeather = tool({
         "The state code only for the US for this city, else return 'none' value",
       ),
   }),
-  execute: async ({ city, country, state }) => {
+  execute: async ({ city, country, state }, { abortSignal }) => {
     const addState = state !== 'none' ? `${state},` : '';
     const query = `q=${city},${addState}${country}&limit=1`;
     const getLocation = await fetch(
       `https://api.openweathermap.org/geo/1.0/direct?${query}&appid=${process.env.OPEN_WEATHER_API_KEY}`,
+      { signal: abortSignal },
     );
 
     const locations = (await getLocation.json()) as {
@@ -34,6 +35,7 @@ export const getWeather = tool({
 
     const response = await fetch(
       `https://api.open-meteo.com/v1/forecast?latitude=${locations[0].lat}&longitude=${locations[0].lon}&current=temperature_2m&hourly=temperature_2m&daily=sunrise,sunset&timezone=auto&forecast_days=3`,
+      { signal: abortSignal },
     );
 
     const weatherData = await response.json();
