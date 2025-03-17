f9edc2bae71c43185e91c3d6b0c42ebad947e3b5
Gwenaël Gallon <gwenael.gallon@mac.com>
2025-03-17 14:09:39 +0100

    feat: remove antialiased class in root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -65,7 +65,7 @@ export default async function RootLayout({
           }}
         />
       </head>
-      <body className="bg-background text-foreground antialiased">
+      <body className="bg-background text-foreground">
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
