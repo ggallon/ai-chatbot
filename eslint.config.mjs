2956bbfdc268426017cfbf84e383ff9feabaf549
Gwenaël Gallon <gwenael.gallon@mac.com>
2025-03-19 22:16:08 +0100

    chore: refactir eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,38 +1,78 @@
-import { FlatCompat } from '@eslint/eslintrc';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import eslintPluginImportX from 'eslint-plugin-import-x';
-//import tailwind from 'eslint-plugin-tailwindcss';
+import eslint from '@eslint/js';
+import pluginJsxA11y from 'eslint-plugin-jsx-a11y';
+import configPrettier from 'eslint-config-prettier';
+import pluginImportX from 'eslint-plugin-import-x';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
-const compat = new FlatCompat({
-  baseDirectory: import.meta.dirname,
-});
-
-const eslintConfig = [
-  ...compat.config({
-    extends: ['next/core-web-vitals', 'next/typescript'],
-  }),
-  eslintPluginImportX.flatConfigs.recommended,
-  eslintPluginImportX.flatConfigs.typescript,
-  //...tailwind.configs['flat/recommended'],
-  eslintConfigPrettier,
+export default tseslint.config(
+  eslint.configs.recommended,
+  tseslint.configs.strict,
+  tseslint.configs.stylistic,
+  pluginJsxA11y.flatConfigs.recommended,
+  pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat['jsx-runtime'],
+  pluginImportX.flatConfigs.recommended,
+  pluginImportX.flatConfigs.typescript,
+  configPrettier,
   {
-    ignores: ['./components/ui/**'],
+    // config with just ignores is the replacement for `.eslintignore`
+    ignores: [
+      '**/*.*.mjs',
+      '.github/',
+      '.next/',
+      '.vercel/',
+      'coverage/',
+      'lib/db/migrations/',
+      'public/',
+      // files
+      'lib/editor/diff.js',
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx', '**/*.mts', '**/*.cts'],
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+        globals: {
+          ...globals.browser,
+          ...globals.nodeBuiltin,
+        },
+      },
+    },
+
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
     rules: {
+      ...pluginReactHooks.configs.recommended.rules,
       'import-x/no-deprecated': 'warn',
+      'react/jsx-no-target-blank': 'off',
+      'react/no-unknown-property': 'off',
+      'react/prop-types': 'off',
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/array-type': 'off',
+      '@typescript-eslint/consistent-type-definitions': 'off',
+      '@typescript-eslint/no-deprecated': 'warn',
+      '@typescript-eslint/no-empty-function': 'warn',
       '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-extraneous-class': 'warn',
+      '@typescript-eslint/no-floating-promises': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unsafe-member-access': 'off',
+      '@typescript-eslint/no-unnecessary-condition': 'warn',
       '@typescript-eslint/no-unused-vars': 'warn',
     },
     settings: {
-      /* no compatibility with v4 yet
-      tailwindcss: {
-        callees: ['clsx', 'cn', 'cx'],
-        config: 'tailwind.config.ts',
-        removeDuplicates: true,
-        whitelist: ['diff-editor'],
+      react: {
+        version: 'detect',
       },
-       */
     },
   },
-];
-
-export default eslintConfig;
+);
