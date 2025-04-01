42345d4f2c93e0f1d171f6540599e4715d5679f6
Gwenaël Gallon <gwenael.gallon@mac.com>
2025-04-01 23:02:27 +0200

    fix: assistant message width

--- components/message.tsx
+++ components/message.tsx
@@ -188,7 +188,7 @@ const PurePreviewMessage = ({
                       </Tooltip>
                     )}
                     {message.role === 'assistant' ? (
-                      <div className="prose flex flex-col dark:prose-invert prose-p:my-2 prose-pre:my-1 prose-pre:bg-(--color-sidebar) prose-pre:p-0 prose-ol:my-0 prose-ul:my-0">
+                      <div className="prose flex max-w-none flex-col dark:prose-invert prose-p:my-2 prose-pre:my-1 prose-pre:bg-(--color-sidebar) prose-pre:p-0 prose-ol:my-0 prose-ul:my-0">
                         <Markdown>{part.text}</Markdown>
                       </div>
                     ) : (
